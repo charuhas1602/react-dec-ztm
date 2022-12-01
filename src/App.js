997f49f6d0f00a8b055d3d4851cc9ea127db06b6
Charuhas Patil <charuhas1602@gmail.com>
2022-12-01 09:05:38 +0530

    setState with Asynch explaination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: {
+        firstName: 'Andrei',
+        lastName: 'Neaogie',
+      },
+      company: 'ZTM',
+    };
+  }
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I'm
+            learning {this.state.company}
+          </a>
+          <button
+            //setState happens asynchronly
+            onClick={() => {
+              this.setState(
+                (state, props) => {
+                  return { name: { firstName: 'Charuhas', lastName: 'Patil' } };
+                },
+                () => {
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
