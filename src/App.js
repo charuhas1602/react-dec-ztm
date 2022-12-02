84b66e0d262e5ff3fd896f15c626827dab7fe48d
Charuhas Patil <charuhas1602@gmail.com>
2022-12-02 07:57:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
@@ -14,40 +13,7 @@ class App extends Component {
     };
   }
   render() {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I'm
-            learning {this.state.company}
-          </a>
-          <button
-            //setState happens asynchronly
-            onClick={() => {
-              this.setState(
-                (state, props) => {
-                  return { name: { firstName: 'Charuhas', lastName: 'Patil' } };
-                },
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change Name
-          </button>
-        </header>
-      </div>
-    );
+    return <div className='App text-danger'>sss</div>;
   }
 }
 
